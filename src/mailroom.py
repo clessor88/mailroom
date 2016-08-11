bf6d3e164eed5486918bf83226c9801a037ec30b
clessor88 <clessor88@yahoo.com>
2016-08-11 16:11:20 -0700

    first test written, function is failing

---
+++ src/mailroom.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+def sort_donors(dict):
+    """Turns donors into list and sorts in decending order."""
+    return dict.values().sort()
---
+++ src/test_mailroom.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+TEST_DICT = {
+    ('lessor', 'crystal'): [10, 40, 25],
+    ('harrison', 'mike'): [30, 40, 15],
+    ('smith', 'john'): [10, 25],
+    ('python', 'monty'): [42]
+}
+
+
+def test_sort_donors():
+    """Sorts donor outputs from most to least donated"""
+    from mailroom import sort_donors
+    result = [('harrison', 'mike'), ('lessor', 'crystal'), ('smith', 'john'), ('python', 'monty')]
+    assert sort_donors(TEST_DICT) == result
