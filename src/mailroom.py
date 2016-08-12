95c7f131baac7ccb46ed936aba5ece399d8f16f1
Mike <mikekh@uw.edu>
2016-08-12 15:36:18 -0700

    Created the get donor_name.

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
+
+
 def sort_donors(donor_dict):
-    """Turns donors into list and sorts in decending order."""
+    """Turn donors into list and sorts in decending order."""
     sorted_donors = sorted(donor_dict, key=lambda x: sum(donor_dict[x]), reverse=True)
     new_list = []
     for k in sorted_donors:
         donor_info = "{}: {}: {}".format(k, donor_dict[k], float(sum(donor_dict[k])) / len(donor_dict[k]))
         new_list.append(donor_info)
-    print(new_list)
     return new_list
+
+# TODO - Create prettify donor_list
+
+
+def donor_name(dictionary):
+    """Get Full Name and Donation Amount."""
+    first_name = input("Please Enter First Name :")
+    last_name = input("Please Enter Last Name :")
+    full_name = (last_name, first_name)
+    amt = input('"Please enter donoation amount :')
+    dictionary.setdeafult(full_name, default=[amt])
+    
--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import sys
+
 TEST_DICT = {
     ('lessor', 'crystal'): [10, 40, 25],
     ('harrison', 'mike'): [30, 40, 15],
@@ -12,3 +14,11 @@ def test_sort_donors():
     from mailroom import sort_donors
     result = ["('harrison', 'mike'): [30, 40, 15]: 28.333333333333332", "('lessor', 'crystal'): [10, 40, 25]: 25.0", "('python', 'monty'): [42]: 42.0", "('smith', 'john'): [10, 25]: 17.5"]
     assert sort_donors(TEST_DICT) == result
+
+def test_donor_name():
+    """Test getting donor name fucntion."""
+    from mailroom import donor_name
+    sys.argv[0] == 'jane'
+    donor_name(TEST_DICT)
+    assert 'jane' in TEST_DICT
+
